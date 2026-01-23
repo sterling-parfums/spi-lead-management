1375e1ed4ddc78be1ebe0b5cdefff5bf74301d9a
Burhanuddin Salim <burhan.salman@gmail.com>
2026-01-23 13:15:36 +0400

    feat: add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { ReactNode } from "react";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>SPI Leads Management</title>
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
